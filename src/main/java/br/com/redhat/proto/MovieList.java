a75ae235aa60f7422427a86dd19c361b439a0fa9
Bruno Cordeiro Mendes <bcordeir@redhat.com>
2026-01-12 17:28:32 -0300

    adiciona protofield

--- src/main/java/br/com/redhat/proto/MovieList.java
+++ src/main/java/br/com/redhat/proto/MovieList.java
@@ -1,8 +1,9 @@
 package br.com.redhat.proto;
 
 import org.infinispan.protostream.annotations.Proto;
+import org.infinispan.protostream.annotations.ProtoField;
 
 import java.util.List;
 
 @Proto
-public record MovieList(List<Movie> movies) {}
+public record MovieList(@ProtoField(number = 1) List<Movie> movies) {}
