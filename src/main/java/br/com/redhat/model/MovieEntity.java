f949e16308e96b020b955128160215c301b97b8c
Bruno Cordeiro Mendes <bcordeir@redhat.com>
2026-01-13 09:48:42 -0300

    Corrige nome tabela

--- src/main/java/br/com/redhat/model/MovieEntity.java
+++ src/main/java/br/com/redhat/model/MovieEntity.java
@@ -1,12 +1,10 @@
 package br.com.redhat.model;
 
 import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
+@Table(name = "movie")
 public class MovieEntity extends PanacheEntityBase {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
